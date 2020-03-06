04681b7b45bd79130dea63d0ef34cdf9c8ae902e
soobing <qls0147@naver.com>
2020-03-06 17:35:59 +0900

    주석처리

--- you-dont-know/5/closure.js
+++ you-dont-know/5/closure.js
@@ -129,16 +129,16 @@
 // }
 
 // 7-3) 해결책 3
-for (var i = 1; i <= 5; i++) {
-  let j = i;
-  setTimeout(function timer() {
-    console.log(j);
-  }, j * 1000)
-}
+// for (var i = 1; i <= 5; i++) {
+//   let j = i;
+//   setTimeout(function timer() {
+//     console.log(j);
+//   }, j * 1000)
+// }
 
 // 7-4) 해결책 4
-for (let i = 1; i <= 5; i++) {
-  setTimeout(function timer() {
-    console.log(i);
-  }, i * 1000)
-}
+// for (let i = 1; i <= 5; i++) {
+//   setTimeout(function timer() {
+//     console.log(i);
+//   }, i * 1000)
+// }
